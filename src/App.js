29eb0ba5401d8fef7f2e3367a27cb8338c6c7e13
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-03-05 23:34:03 +0000

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
+import {useState} from 'react';
 import './App.css';
 
+
+
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  const [tweets, setTweets] = useState([]);
+
+  const handleMessageChange = event => {
+    // 👇️ access textarea value
+    setMessage(event.target.value);
+    createThread(event.target.value)
+    // console.log(event.target.value);
+  };
+
+
+  function createThread(text) {
+    // Split by three dashes at the beginning of a line
+    const thread = text.split(/^-/gm);
+    // Remove empty spaces and new lines from the array
+    const threadClean = thread.filter(item => item.trim() !== '').map(item => item.trim());
+    // Create an array of objects with an increasing Id  and the text
+    const count = threadClean.length;
+    const threadObj = threadClean.map((item, index) => {
+      return {
+        id: index,
+        text: `${index+1}/${count} ${item}`
+      }
+    });
+
+    setTweets(threadObj);
+  }
+
+  const tweetStyle = {
+    border: '1px solid black',
+    padding: '10px',
+    margin: '10px'
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="message"
+        name="message"
+        value={message}
+        onChange={handleMessageChange}
+      />
+      <div>
+        {tweets.map(tweet => (
+          <div style={tweetStyle} key={tweet.id}>{tweet.text}</div>
+        ))}
+      </div>
     </div>
   );
 }
